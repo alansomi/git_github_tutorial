b9f10623bac8278f33b935b04242371da320749f
Alan <alansomi76@gmail.com>
2025-12-10 17:47:31 +0530

    Added calculator file

---
+++ calculator.py
@@ -0,0 +1,20 @@
+#  Simple Calculator program
+''' This is a program contains arithmetic operations (basic level)'''
+import platform 
+
+def add(a,b):
+    return a + b 
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divided(a,b):
+    return a / b
+
+print("addition :",add(199,346))
+print("subtraction:",subtract(569,412))
+print("multiplication:",multiply(478,765))
+print("division:",divided(343,3))
