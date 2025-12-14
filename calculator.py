3a5b73d9694375fa70088b6db2a7abaf01c42311
Alan <alansomi76@gmail.com>
2025-12-14 13:02:04 +0530

    Modified calculator file

--- calculator.py
+++ calculator.py
@@ -2,19 +2,34 @@
 ''' This is a program contains arithmetic operations (basic level)'''
 import platform 
 
-def add(a,b):
-    return a + b 
+n1 = int(input("Enter the first number    :"))
+n2 = int(input("Enter the second number   :"))
 
-def subtract(a,b):
-    return a - b
+def addition(n1,n2):
+    return n1 + n2
+def subtract(n1,n2):
+    return n1 - n2 
+def multiply(n1,n2):
+    return n1 * n2
+def division(n1,n2):
+    return n1 / n2
 
-def multiply(a,b):
-    return a * b
 
-def divided(a,b):
-    return a / b
+choices = "1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n" 
+print("choices you have :",choices)
 
-print("addition :",add(199,346))
-print("subtraction:",subtract(569,412))
-print("multiplication:",multiply(478,765))
-print("division:",divided(343,3))
+choice = input("enter choice/1/2/3/4 :")
+print("you entered choice :",choice)
+
+if choice == "1":
+   result = addition(n1,n2)
+   print("Addition    :", result)
+elif choice == "2":
+    result = subtract(n1,n2 )
+    print("Subtraction   :",result)
+elif choice == "3":
+    result = multiply(n1,n2)
+    print("Multiplication of inputs   :",result)
+elif choice == "4":
+    result = division(n1,n2)
+    print("Division of inputs   :",result)
